903e54b835bd61c35c7d39856a22646c2d4ac739
Omar Assem <omarassem.route6@gmail.com>
2024-09-24 02:02:13 +0300

    Implement auth

---
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce/features/auth/data/models/login_request.dart';
+import 'package:ecommerce/features/auth/data/models/register_request.dart';
+import 'package:ecommerce/features/auth/data/repositories/auth_repository.dart';
+import 'package:ecommerce/features/auth/presentation/cubit/auth_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+@singleton
+class AuthCubit extends Cubit<AuthState> {
+  final AuthRepository authRepository;
+
+  AuthCubit(
+    this.authRepository,
+  ) : super(AuthInitial());
+
+  Future<void> register(RegisterRequest request) async {
+    emit(RegisterLoading());
+    final result = await authRepository.register(request);
+    result.fold(
+      (failure) => emit(RegisterError(failure.message)),
+      (_) => emit(RegisterSuccess()),
+    );
+  }
+
+  Future<void> login(LoginRequest request) async {
+    emit(LoginLoading());
+    final result = await authRepository.login(request);
+    result.fold(
+      (failure) => emit(LoginError(failure.message)),
+      (_) => emit(LoginSuccess()),
+    );
+  }
+}
